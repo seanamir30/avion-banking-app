a1d98dd7df52b6da6d8091daedff45e8e94145cf
John Carreos <jrcarreos1@gmail.com>
2021-09-04 10:41:49 +0800

    changed the table component to function based

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -1,43 +1,41 @@
 import React, { Component } from "react";
-class Accounts extends Component {
-  state = {
-    accounts: [
-      { id: 1, name: "Sample Name", balance: 9999 },
-      { id: 2, name: "Your Name", balance: 8888 },
-      { id: 3, name: "My Name", balance: 12345 },
-    ],
-  };
+import { useState } from "react";
 
-  render() {
-    return (
-      <React.Fragment>
-        <h2 className="text-center">All Accounts</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Account Number</th>
-              <th>Balance</th>
-              <th />
+const Accounts = () => {
+  const [accounts, setAccount] = useState([
+    { id: 1, name: "Sample Name", balance: 9999 },
+    { id: 2, name: "Your Name", balance: 8888 },
+    { id: 3, name: "My Name", balance: 12345 },
+  ]);
+
+  return (
+    <React.Fragment>
+      <h2 className="text-center">All Accounts</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Account Number</th>
+            <th>Balance</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {accounts.map((account) => (
+            <tr key={account.id}>
+              <td>{account.name}</td>
+              <td>{account.id}</td>
+              <td>{account.balance}</td>
+              <td>
+                <button className="btn btn-danger">Manage</button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {this.state.accounts.map((account) => (
-              <tr key={account.id}>
-                <td>{account.name}</td>
-                <td>{account.id}</td>
-                <td>{account.balance}</td>
-                <td>
-                  <button class="btn btn-danger">Manage</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <button class="btn btn-success">Add Account</button>
-      </React.Fragment>
-    );
-  }
-}
+          ))}
+        </tbody>
+      </table>
+      <button className="btn btn-success">Add Account</button>
+    </React.Fragment>
+  );
+};
 
 export default Accounts;
