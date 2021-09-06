0f2fd4d29de675d4c70fb6fc53cbe7b4b997c73f
John Carreos <jrcarreos1@gmail.com>
2021-09-06 17:03:06 +0800

    Components for withdraw, deposit, and transfer are added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import LoginPage from './components/LoginPage';
-import Accounts from './components/Accounts';
-import HomePage from './components/HomePage';
+import LoginPage from "./components/LoginPage";
+import Admin from "./components/Admin.js";
+import HomePage from "./components/HomePage";
+import AdminTransaction from "./components/AdminTransaction.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
-
+import "./App.css";
 
 // Sample local storage contents
-window.localStorage.setItem('test@gmail.com', JSON.stringify({ 'password': '12345', 'isAdmin': true }))
-window.localStorage.setItem('idk@gmail.com', JSON.stringify({ 'password': 'password', 'isAdmin': false, 'balance': 18000, 'id': 8888222311, 'name': 'Sean' }))
-window.localStorage.setItem('idk1@gmail.com', JSON.stringify({ 'password': '11111', 'isAdmin': false, 'balance': 321000, 'id': 1234567, 'name': 'John' }))
+window.localStorage.setItem(
+  "test@gmail.com",
+  JSON.stringify({ password: "12345", isAdmin: true })
+);
+window.localStorage.setItem(
+  "idk@gmail.com",
+  JSON.stringify({
+    password: "password",
+    isAdmin: false,
+    balance: 18000,
+    id: 8888222311,
+    name: "Sean",
+  })
+);
+window.localStorage.setItem(
+  "idk1@gmail.com",
+  JSON.stringify({
+    password: "11111",
+    isAdmin: false,
+    balance: 321000,
+    id: 1234567,
+    name: "John",
+  })
+);
 
 function App() {
   return (
     <Router>
       <div className="App">
-          <Switch>
-            <Route exact path="/" component={LoginPage} />
-            <Route exact path="/home" component={HomePage} />
-            <Route exact path="/admin" component={Accounts}/>
-          </Switch>
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/home" component={HomePage} />
+          <Route exact path="/admin" component={Admin} />
+          <Route
+            exact
+            path="/manage-transaction"
+            component={AdminTransaction}
+          />
+        </Switch>
       </div>
     </Router>
   );
--- src/components/loginPage.js
+++ src/components/loginPage.js
@@ -2,68 +2,75 @@ import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 const LoginPage = () => {
+  let history = useHistory();
+  // Handles Login
+  const handleLogin = () => {
+    const verify = JSON.parse(localStorage.getItem(email));
 
-    let history = useHistory();
-    // Handles Login
-    const handleLogin = () => {
-        const verify = JSON.parse(localStorage.getItem(email));
-        
-        if (verify && password === verify.password) {
+    if (verify && password === verify.password) {
+      // Checks if the user is an Admin
+      if (verify.isAdmin) {
+        history.push({ pathname: "/admin", state: { verify } });
+        console.log("An admin logged in!");
+      } else {
+        history.push({ pathname: "/home", state: { verify } });
+        console.log("Logged in!");
+      }
+      //
+    } else console.log("try again :(");
+  };
 
-            // Checks if the user is an Admin
-            if (verify.isAdmin) {
-                history.push({pathname: '/admin', state:{verify}})
-                console.log('An admin logged in!');
-            } else {
-                history.push({pathname: '/home', state:{verify}})
-                console.log('Logged in!')
-            }
-            //
+  //
 
+  // This is where the user's input from the form is saved
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //
 
-        }
-        else (
-            console.log('try again :(')
-        )
-    }
-
-    //
-
-
-
-    // This is where the user's input from the form is saved
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    //
-
-
-
-    return (
-        <div className="vh-100">
-            <div className="container py-5 h-100">
-                <div className="row d-flex align-items-center justify-content-center h-100">
-                    <div className="col-md-8 col-log-7 col-xl-7 text-center">
-                        *Insert something*
-                    </div>
-                    <div className="col-md-7 col-lg-5 col-xl-5">
-                        <form onSubmit={handleLogin}>
-                            <div className="form-floating mb-4">
-                                <input type="email" id="emailInput" className="form-control form-control-lg"
-                                    placeholder="Enter your email here!" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                <label className="form-label" htmlFor="emailInput">Email address</label>
-                            </div>
-                            <div className="form-floating mb-4">
-                                <input type="password" id="passwordInput" className="form-control form-control-lg"
-                                    placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                                <label className="form-label" htmlFor="passwordInput">Password</label>
-                            </div>
-                            <button type="submit" className="btn btn-danger">LOG IN</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="vh-100">
+      <div className="container py-5 h-100">
+        <div className="row d-flex align-items-center justify-content-center h-100">
+          <div className="col-md-8 col-log-7 col-xl-7 text-center">
+            *Insert something*
+          </div>
+          <div className="col-md-7 col-lg-5 col-xl-5">
+            <form onSubmit={handleLogin}>
+              <div className="form-floating mb-4">
+                <input
+                  type="email"
+                  id="emailInput"
+                  className="form-control form-control-lg"
+                  placeholder="Enter your email here!"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label className="form-label" htmlFor="emailInput">
+                  Email address
+                </label>
+              </div>
+              <div className="form-floating mb-4">
+                <input
+                  type="password"
+                  id="passwordInput"
+                  className="form-control form-control-lg"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="form-label" htmlFor="passwordInput">
+                  Password
+                </label>
+              </div>
+              <button type="submit" className="btn btn-danger">
+                LOG IN
+              </button>
+            </form>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
