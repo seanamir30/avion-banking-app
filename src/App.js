370009eaed02a2ea7ed2a45c0df528717565504b
Sean Cafe <seanamir30@gmail.com>
2021-09-06 20:28:54 +0800

    Removed sample local storage items, and made 1 admin account

--- src/App.js
+++ src/App.js
@@ -5,31 +5,16 @@ import AdminTransaction from "./components/AdminTransaction.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-// Sample local storage contents
+
+// admin login
 window.localStorage.setItem(
-  "test@gmail.com",
-  JSON.stringify({ password: "12345", isAdmin: true })
+  "admin@gmail.com",
+  JSON.stringify({ password: "admin", isAdmin: true })
 );
-window.localStorage.setItem(
-  "idk@gmail.com",
-  JSON.stringify({
-    password: "password",
-    isAdmin: false,
-    balance: 18000,
-    id: 8888222311,
-    name: "Sean",
-  })
-);
-window.localStorage.setItem(
-  "idk1@gmail.com",
-  JSON.stringify({
-    password: "11111",
-    isAdmin: false,
-    balance: 321000,
-    id: 1234567,
-    name: "John",
-  })
-);
+
+
+
+
 
 function App() {
   return (
