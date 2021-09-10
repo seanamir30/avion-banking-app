59a8f8feeae40b4ae8be1ddcf0ebebea666adc2b
Sean Cafe <seanamir30@gmail.com>
2021-09-10 19:14:01 +0800

    Added buttons at homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,24 @@
 import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import Card from './Card';
 
 function HomePage() {
     const passedState = useLocation();
-    const {state} = passedState;
+    const { state } = passedState;
     const user = state.verify;
     console.log(user)
 
     return (
-        <div className="container">
+        <div className="container vh-100">
             <div className="d-flex flex-column align-items-center">
-                <i class="fas fa-th-large align-self-start mt-3"></i>
-                <Card user={user}/>
-                <i class="fas fa-chart-line align-self-end m-2"></i>
+                <i className="fas fa-th-large align-self-start mt-3"></i>
+                <Card user={user} />
+                <i className="fas fa-chart-line align-self-end m-2"></i>
+                <div className="btn-group" role="group" aria-label="Nav">
+                    <button type="button" className="btn btn-outline-danger">Widthdraw</button>
+                    <button type="button" className="btn btn-outline-danger">Deposit</button>
+                    <button type="button" className="btn btn-outline-danger">Transfer</button>
+                </div>
             </div>
         </div>
     )
