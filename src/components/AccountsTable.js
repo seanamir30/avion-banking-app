0fdaeda8a795a0e31581ea3deea33b93cd38abff
John Carreos <jrcarreos1@gmail.com>
2021-09-13 20:53:23 +0800

    modified the delete function to deleting an account without reloading

--- src/components/AccountsTable.js
+++ src/components/AccountsTable.js
@@ -1,38 +1,41 @@
-import { useState,useRef } from "react";
+import { useState, useRef } from "react";
 
+const AccountsTable = (props) => {
+  let accounts = [];
+  // const hasUpdated = useRef(false)
+  // const [updateAccounts,setUpdateAccounts] = useState([])
+  console.log(accounts);
+  for (let i = 0; i < localStorage.length; i++) {
+    let account = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    if (!account.isAdmin) accounts.push(account);
+  }
 
-const AccountsTable = () => {
-    
-    let accounts = []
-    // const hasUpdated = useRef(false)
-    // const [updateAccounts,setUpdateAccounts] = useState([])
+  const [allAccounts, setAllAccounts] = useState(accounts);
 
-    for (let i = 0; i < localStorage.length; i++) {
-        let account = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        if (!account.isAdmin) accounts.push(account);
-    }
+  const handleDelete = (key) => {
+    localStorage.removeItem(key);
+    setAllAccounts([...accounts]);
+  };
 
-    // if(!hasUpdated.current){
-    //     setUpdateAccounts(accounts)
-    //     hasUpdated.current = true;
-    // }
-    const handleDelete = (key) => {
-        localStorage.removeItem(key);
-        window.location.reload();
-    };
+  return (
+    <tbody>
+      {allAccounts.map((account) => (
+        <tr key={account.id}>
+          <td>{account.name}</td>
+          <td>{account.id}</td>
+          <td>{account.balance}</td>
+          <td>
+            <i
+              class="fa fa-trash"
+              style={{ cursor: "pointer" }}
+              onClick={() => handleDelete(account.id)}
+              aria-hidden="true"
+            ></i>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+};
 
-    return (
-        <tbody>
-            {accounts.map((account) => (
-                <tr id={account.id}>
-                    <td>{account.name}</td>
-                    <td>{account.id}</td>
-                    <td>{account.balance}</td>
-                    <td><i class="fa fa-trash" style={{cursor:"pointer"}} onClick={() => handleDelete(account.id)} aria-hidden="true"></i></td>
-                </tr>
-            ))}
-        </tbody>
-    )
-}
-
-export default AccountsTable
+export default AccountsTable;
