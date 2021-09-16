170b7ce9a9a7da40172d70c09d136fb5fd83c586
Sean Cafe <seanamir30@gmail.com>
2021-09-17 00:33:02 +0800

    Accounts table updates without refreshing

--- src/components/AccountsTable.js
+++ src/components/AccountsTable.js
@@ -1,28 +1,8 @@
 import { useState, useRef } from "react";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 
-const AccountsTable = (props) => {
-  let accounts = [];
-  // const hasUpdated = useRef(false)
-  // const [updateAccounts,setUpdateAccounts] = useState([])
+const AccountsTable = ({updateTable,allAccounts}) => {
 
-  const [allAccountsChecked, setAllAccountsChecked] = useState(false);
-  const [allAccounts, setAllAccounts] = useState(accounts);
-
-  if (!allAccountsChecked) {
-    for (let i = 0; i < localStorage.length; i++) {
-      let account = JSON.parse(localStorage.getItem(localStorage.key(i)));
-      if (!account.isAdmin) accounts.push(account);
-    }
-    setAllAccountsChecked(true);
-    setAllAccounts([...accounts]);
-  }
-
-  const updateTable = () => {
-    setAllAccounts([...accounts]);
-    setAllAccountsChecked(false);
-    console.log("update");
-  };
 
   const handleDelete = (key) => {
     localStorage.removeItem(key);
